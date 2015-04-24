88bd4ae0654af314c5b8daa4cabb1c39f4fbe26a
ctg123 <chaanceg123@gmail.com>
2015-04-24 16:59:32 -0500

    Create challenge1.py

---
+++ Students/ctg123/challenge1.py
@@ -0,0 +1,17 @@
+import random
+
+SampleSpaceSize = 2 # bits can only take 2 values: 0 or 1.
+NumberTrials = 10000
+
+TrialSequence = [] # first column
+for TrialIndex in range(0, NumberTrials):
+    TrialSequence.append(random.randrange(SampleSpaceSize))
+
+    # Then, look at the empirical distribution of the ratios of zeros and ones.
+
+    percent = [] # second column 
+    for OutcomeIndex in range(0, SampleSpaceSize):
+        percent.append(TrialSequence.count(OutcomeIndex)/ float(NumberTrials))
+    print(percent)
+
+    # Explore how the empirical distribution changes as N increases 10.0, 100.0, 1000.0, 10000.0.
