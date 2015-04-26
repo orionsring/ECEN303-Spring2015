4594f354b657e9ded93753e570f84826d249092f
jordankilburn <jordantyeburn@email.tamu.edu>
2015-04-26 17:00:52 -0500

    Add problem 1

---
+++ Students/jordankilburn/Problem1.py
@@ -0,0 +1,14 @@
+import random
+
+SampleSpaceSize = 10
+NumberTrials = 10000
+
+TrialSequence = []
+for TrialIndex in range(0, NumberTrials):
+	TrialSequence.append(random.randrange(SampleSpaceSize))
+
+percent = []
+for OutcomeIndex in range(0, SampleSpaceSize):
+	percent.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
+print(percent)
+print(TrialSequence)
